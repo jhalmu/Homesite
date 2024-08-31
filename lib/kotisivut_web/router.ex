f22b276fe16d8a219069b54120c88982dd7363c6
Juha Halmu <nospam@email.com>
2024-09-01 01:05:23 +0300

    now  one can see one post too

--- lib/kotisivut_web/router.ex
+++ lib/kotisivut_web/router.ex
@@ -23,6 +23,7 @@ defmodule KotisivutWeb.Router do
     get "/", PageController, :home
 
     live "/posts", PostLive.Index, :index
+    live "/posts/:id", PostLive.Show, :show
   end
 
   # Other scopes may use custom stacks.
@@ -72,7 +73,6 @@ defmodule KotisivutWeb.Router do
       live "/users/settings/confirm_email/:token", UserSettingsLive, :confirm_email
       live "/posts/new", PostLive.Index, :new
       live "/posts/:id/edit", PostLive.Index, :edit
-      live "/posts/:id", PostLive.Show, :show
       live "/posts/:id/show/edit", PostLive.Show, :edit
     end
   end
