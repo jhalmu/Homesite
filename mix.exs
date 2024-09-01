0da59457197635680b5cd3587d3b5b159b90e86c
Juha Halmu <nospam@email.com>
2024-09-01 17:41:49 +0300

    Added Dialyzer

--- mix.exs
+++ mix.exs
@@ -60,7 +60,8 @@ defmodule Kotisivut.MixProject do
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
       {:bandit, "~> 1.5"},
-      {:credo, "~> 1.7", only: [:dev, :test], runtime: false}
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.4", only: [:dev], runtime: false}
     ]
   end
 
